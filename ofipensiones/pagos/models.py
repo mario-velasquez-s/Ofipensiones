db14ce5b20211d84f9eacd0972ad8c617589210d
Juaneslot01 <j.lopezt@uniandes.edu.co>
2024-10-06 21:58:25 -0500

    Terminado atributos Pago

--- ofipensiones/pagos/models.py
+++ ofipensiones/pagos/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from estudiante.models import Estudiante
+from acudiente.models import Acudiente
 
 class Pagos(models.Model):
     codigoPago = models.BigIntegerField()
     monto = models.BigIntegerField()
     fechaPago = models.DateField()
-    estudiante = models.BigIntegerField()
-    padre = models.BigIntegerField()
-    
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.DO_NOTHING)
+    acudiente = models.ForeignKey(Acudiente, on_delete=models.DO_NOTHING)
 
 
     def __str__(self):
